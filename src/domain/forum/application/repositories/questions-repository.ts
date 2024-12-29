79f499dce64c93d6d20080cd5cb8bf6730b989ac
Dante <beyondmind9@gmail.com>
2024-12-29 10:33:31 -0300

    use case list paginated questions

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,7 +1,9 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '../../enterprise/entities/question'
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | undefined>
+  findManyRecent(params: PaginationParams): Promise<Question[]>
   create(question: Question): Promise<void>
   save(question: Question): Promise<void>
   findBySlug(slug: string): Promise<Question | undefined>
